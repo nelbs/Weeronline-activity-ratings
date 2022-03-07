7cd67066bf92d01fe21ac90888b59ebaceda4335
Nick Elbers <nick.elbers@rhdhv.com>
2022-03-07 10:33:24 +0100

    Update sensor.py

--- custom_components/weatherrating/sensor.py
+++ custom_components/weatherrating/sensor.py
@@ -64,7 +64,7 @@ class Weatherrating(RestoreEntity):
         return self._state
 
     @property
-    def device_state_attributes(self):
+    def extra_state_attributes(self):
         # Return the state attributes.
         return self._attributes
 
